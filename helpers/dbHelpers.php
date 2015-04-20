85559a9f68cc694d80180abf9d82d9de99420a90
Abhijeet Mohan <void.aby@gmail.com>
2015-04-20 23:24:43 +0530

    removed junk

--- helpers/dbHelpers.php
+++ helpers/dbHelpers.php
@@ -2,7 +2,6 @@
 <?php
     function dbConnect(){
         try{
-<<<<<<< HEAD
 			// Localhost
             /*$username = 'root';
             $password = '';
@@ -13,11 +12,9 @@
             $password = 'cAsUpXKJkz5n';
             $conn = new pdo("mysql:host=127.7.184.2;dbname=accounts", $username, $password);
 			
-=======
             $username = 'root';
             $password = '';
             $conn = new pdo("mysql:host=localhost;dbname=accountsdb;", $username, $password);
->>>>>>> 19b68c5484f0d6a5cfb7e753940d1d564f7e1af6
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
 
